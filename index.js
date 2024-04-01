4a4d77be39f3504b2938466812e8b5fb785a7050
Bruno Pagno <pagno.bruno@bcg.com>
2024-04-01 12:58:23 -0300

    implemented JWT auth

--- index.js
+++ index.js
@@ -1,17 +1,21 @@
 const express = require("express");
+const jwt = require("jsonwebtoken");
+
 const app = express();
 const port = 3000;
+const JWT_SECRET = "lorem";
 
 function basicAuthMiddleware(req, res, next) {
   const authHeader = req.headers["authorization"];
 
   if (!authHeader) return unauthorized(res);
 
-  const base64Credentials = authHeader.split(" ")[1];
+  const [prefix, base64Credentials] = authHeader.split(" ")[1];
+  if (prefix !== "Basic") return res.status(401).send("Invalid Token prefix");
+
   const credentials = Buffer.from(base64Credentials, "base64").toString();
   const [username, password] = credentials.split(":");
 
-  // Example of username and password
   // YWRtaW46cGFzc3dvcmQ= for admin:password for the enconded base64 version of it
   if (username === "admin" && password === "password") {
     next();
@@ -20,13 +24,32 @@ function basicAuthMiddleware(req, res, next) {
   }
 }
 
+function jwtAuthMiddleware(req, res, next) {
+  const authHeader = req.headers["authorization"];
+  if (!authHeader) return unauthorized(res);
+
+  const [prefix, token] = authHeader.split(" ");
+  if (prefix !== "Bearer") return res.status(401).send("Invalid Token prefix");
+
+  try {
+    const decoded = jwt.verify(token, JWT_SECRET);
+    req.user = decoded;
+  } catch (err) {
+    return res.status(401).send("Invalid Token");
+  }
+  return next();
+}
+
 function unauthorized(res) {
-  res.setHeader("WWW-Authenticate", 'Basic realm="Example"');
   return res.status(401).send("Unauthorized");
 }
 
-app.get("/protected", basicAuthMiddleware, (req, res) => {
-  res.send("Basic authentication worked");
+app.get("/basic-protected", basicAuthMiddleware, (req, res) => {
+  res.send("Basic authentication succeeded");
+});
+
+app.get("/jwt-protected", jwtAuthMiddleware, (req, res) => {
+  res.send("JWT authentication succeeded");
 });
 
 app.listen(port, () => {
